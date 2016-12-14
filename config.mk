fd658f5763c32415d3056fb37338473f848bea96
lachlan <lachlan.deer@uzh.ch>
2016-12-14 14:00:22 -0800

    initiated a git repository for box office mojo scrape

---
+++ config.mk
@@ -0,0 +1,12 @@
+PROJECT_ROOT = .
+
+# Source File Locations
+SRC        = $(PROJECT_ROOT)/src
+IN_SEARCH  = $(SRC)/buildSearch
+
+
+# Output Locations
+OUT                 = $(PROJECT_ROOT)/out
+OUT_EMPTYSEARCH     = $(OUT)/emptySearches
+OUT_INTERMEDIATE    = $(OUT)/intermediateQuery
+OUT_FINAL           = $(OUT)/finalSearch
