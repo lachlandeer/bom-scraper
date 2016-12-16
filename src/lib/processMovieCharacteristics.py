248f1d13ce44c221e87e8bf6d97e216f2717110e
lachlan <lachlan.deer@uzh.ch>
2016-12-15 17:35:48 -0800

    initialized a file to store functions to scrape film characteristics from summary pages of box office mojo

---
+++ src/lib/processMovieCharacteristics.py
@@ -0,0 +1,6 @@
+"""
+This collection of functions scrapes most of the important data about movie
+observable characteristics from the film's summary page on Box Office Mojo.
+
+Last Run: December, 2016
+"""
